b2fd7da31cf5f10341e66e1361f51c012ed4b86e
bachiito <riveraabraham332@gmail.com>
2022-10-14 12:58:05 -0400

    styles changed and pseudo classes added

--- js/index.js
+++ js/index.js
@@ -1,21 +1,42 @@
+let salary = 0;
+const AFPDiscount = 2.87;
+const SFSDiscount = 3.04;
+const year = new Date().getFullYear();
+const requiredInputId = "monthly-salary";
+
 const form = document.querySelector("form");
 const inputGroups = document.querySelectorAll(".input-group");
+const result = document.querySelector(".result");
 const copyrightYear = document.querySelector(".year");
-const year = new Date().getFullYear();
-const numRegex = /\d/g;
 
 copyrightYear.textContent = year;
 
 form.addEventListener("submit", (e) => {
+    let validInputs = true;
     e.preventDefault();
-    inputGroups.forEach((inputGroup) => {
-        const input = inputGroup.querySelector("input[type='text']");
-        const errorMsg = inputGroup.querySelector(".alert-msg");
 
-        if (!input.value.match(numRegex)) {
-            errorMsg.textContent = "Please enter only numbers.";
+    inputGroups.forEach((inputGroup) => {
+        const input = inputGroup.querySelector("input");
+        const alertMsg = inputGroup.querySelector(".alert-msg");
+
+        if (isNaN(input.value)) {
+            alertMsg.classList.remove("d-none");
+            validInputs = false;
         } else {
-            errorMsg.textContent = "";
+            alertMsg.classList.add("d-none");
+        }
+
+        if (input.getAttribute("id") === requiredInputId && !input.value) {
+            validInputs = false;
+            alertMsg.textContent = "This field is required.";
+            alertMsg.classList.remove("d-none");
         }
     });
+
+    if (validInputs) {
+        salary = document.querySelector(`#${requiredInputId}`).value;
+        result.textContent = `"The inputs given are valid" OUR SALAY IS $RDS ${salary}`;
+    } else {
+        result.textContent = "The inputs given are NOT valid";
+    }
 });

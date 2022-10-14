fb012dbb91715ad486cd811a4bf14fb552dfb175
bachiito <riveraabraham332@gmail.com>
2022-10-14 09:11:31 -0400

    page styles ready

--- js/index.js
+++ js/index.js
@@ -1,4 +1,21 @@
-const year = new Date().getFullYear();
+const form = document.querySelector("form");
+const inputGroups = document.querySelectorAll(".input-group");
 const copyrightYear = document.querySelector(".year");
+const year = new Date().getFullYear();
+const numRegex = /\d/g;
 
 copyrightYear.textContent = year;
+
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    inputGroups.forEach((inputGroup) => {
+        const input = inputGroup.querySelector("input[type='text']");
+        const errorMsg = inputGroup.querySelector(".alert-msg");
+
+        if (!input.value.match(numRegex)) {
+            errorMsg.textContent = "Please enter only numbers.";
+        } else {
+            errorMsg.textContent = "";
+        }
+    });
+});
